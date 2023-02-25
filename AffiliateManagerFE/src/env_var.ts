a9d95f67e9edd7edd8290a3e70c0393d4561c604
aaronangxz <angxze@gmail.com>
2023-02-25 19:21:49 +0800

    Add env var for staging

--- AffiliateManagerFE/src/env_var.ts
+++ AffiliateManagerFE/src/env_var.ts
@@ -1,3 +1,8 @@
 export default {
-  Env: import.meta.env.VITE_ENV === 'PROD' ? 'https://affiliatemanager-378714.as.r.appspot.com' : 'http://127.0.0.1:8888',
+  Env:
+    import.meta.env.VITE_ENV === 'PROD'
+      ? 'https://affiliatemanager-378714.as.r.appspot.com'
+      : import.meta.env.VITE_ENV === 'STAGING'
+      ? 'https://affiliatemanagerstaging.as.r.appspot.com'
+      : 'http://127.0.0.1:8888',
 };
